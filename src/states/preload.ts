a1a84bebffe1283dea6bdafa257f77af36e3965f
Johan Lindfors <johan.lindfors@coderox.se>
2018-05-17 14:26:53 +0200

    Clean assets, except font

--- src/states/preload.ts
+++ src/states/preload.ts
@@ -13,22 +13,29 @@ namespace FlappyBird {
 
             // images
             this.load.image("sky", "assets/sky.png");
-            this.load.image("ground", "assets/ground.png");
-            this.load.image("title", "assets/title.png");
-            this.load.image("startButton", "assets/start-button.png");
-            this.load.image("instructions", "assets/instructions.png");
-            this.load.image("getReady", "assets/get-ready.png");
-            this.load.image("scoreboard", "assets/scoreboard.png");
-            this.load.image("gameover", "assets/gameover.png");
+            this.load.image("title", "assets/title_clean.png");
+            this.load.image("startButton", "assets/start-button_clean.png");
+            this.load.image("instructions", "assets/instructions_clean.png");
+            this.load.image("getReady", "assets/get-ready_clean.png");
+            this.load.image("scoreboard", "assets/scoreboard_clean.png");
+            this.load.image("gameover", "assets/gameover_clean.png");
             this.load.image("particle", "assets/particle.png");
-            this.load.image("trees", "assets/trees.png");
-            this.load.image("clouds", "assets/clouds.png");
-            this.load.image("cityscape", "assets/cityscape.png");
-        
+            //this.load.image("trees", "assets/trees.png");
+            //this.load.image("clouds", "assets/clouds.png");
+            //this.load.image("cityscape", "assets/cityscape.png");
+            //this.load.image("ground", "assets/ground.png");
+            
+            this.load.image("trees", "assets/trees_clean.png");
+            this.load.image("clouds", "assets/clouds_clean.png");
+            this.load.image("cityscape", "assets/cityscape_clean.png");
+            this.load.image("ground", "assets/ground_clean.png");
+            
             // sprite sheets
-            this.load.spritesheet("medals", "assets/medals.png",44, 46, 2);
-            this.load.spritesheet("bird", "assets/bird.png", 34,24,3);
-            this.load.spritesheet("pipe", "assets/pipes.png", 54,320,2);
+            this.load.spritesheet("medals", "assets/medals_clean.png",44, 46, 2);
+            //this.load.spritesheet("bird", "assets/bird.png", 34,24,3);
+            //this.load.spritesheet("pipe", "assets/pipes.png", 54,320,2);
+            this.load.spritesheet("bird", "assets/bird_clean.png", 25,25,3);
+            this.load.spritesheet("pipe", "assets/pipes_clean.png", 54,320,2);
 
             // sound effects
             this.load.audio("flap", "assets/flap.wav");
